5a99e0bc672e65e69ccf6211e92533d7ca57f916
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-08-09 14:01:49 +0200

    Modul 11: Generische Listen + Lab

--- GenerischeListen/Program.cs
+++ GenerischeListen/Program.cs
@@ -1,5 +1,4 @@
-﻿using Fahrzeugpark;
-using Lab06f_Fahrzeugpark;
+﻿using M06f_OOP;
 
 namespace GenerischeListen
 {
@@ -7,44 +6,57 @@ namespace GenerischeListen
     {
         static void Main(string[] args)
         {
-            List<string> Städteliste = new List<string>()
+            //Deklaration und Initialisierung einer Liste von Strings
+            System.Collections.Generic.List<string> Städteliste = new List<string>()
             {
-                "Berlin",
-                "Hamburg",
-                "München"
+                "Nürnberg",
+                "Dortmund",
+                "Bielefeld"
             };
 
-            Städteliste.Add("Düsseldorf");
+            //Hinzufügen von Listeneinträgen
+            Städteliste.Add("Hamburg");
+            Städteliste.Add("Berlin");
+            Städteliste.Add("München");
             Städteliste.Add("Köln");
-            Städteliste.Add("Nürnberg");
+            Städteliste.Add("Düsseldorf");
 
+            //Ausgabe der Länge der Liste
             Console.WriteLine(Städteliste.Count);
 
-            Console.WriteLine(Städteliste[2]);
-            Städteliste[4] = "Bielefeld";
+            //Ausgabe der 4. Listenposition
+            Console.WriteLine(Städteliste[3]);
 
+            //Manipulation der 4. Listenposition
+            Städteliste[3] = "Dresden";
+            Console.WriteLine(Städteliste[3]);
+
+            //Schleife über die Liste
             foreach (var item in Städteliste)
             {
                 Console.WriteLine(item);
             }
 
-            Städteliste.Remove("München");
+            //Löschen des Eintrags 'Köln' (Nachfolgende Einträge rücken nach oben)
+            Städteliste.Remove("Köln");
 
+            //Beispielliste mit Lebewesen
+            List<Lebewesen> lebewesenListe = new List<Lebewesen>();
+            lebewesenListe.Add(new Mensch("Anna", "Meier", "Lasagne", new DateTime(1984, 5, 6), 189, false));
+            lebewesenListe.Add(new Mensch("Rainer", "Zufall", "Lasagne", new DateTime(1984, 5, 6), 189, true));
+            Console.WriteLine(lebewesenListe[1].Name);
 
-            List<Fahrzeug> fahrzeuge = new List<Fahrzeug>()
-            {
-                new PKW("BMW", 250, 23000, 5),
-                Fahrzeug.GeneriereFahrzeug()
-            };
+            //Deklaration und Initialisierung eines Dictionarys (Key: String, Value: Int)
+            Dictionary<string, int> Dict = new Dictionary<string, int>();
 
-
-            Dictionary<string, int>Dict = new Dictionary<string, int>();
+            //Hinzufügen von Dictionary-Einträgen
             Dict.Add("Hallo", 456);
-            Dict.Add("Ahoj", -12);
-            Dict.Add("Moin", 0);
-            Dict.Add("Ciao", 99999);
+            Dict.Add("Ahoj", 5);
+            Dict.Add("Ciao", -78);
+            Dict.Add("Moin", 456);
 
-            Console.WriteLine(Dict["Moin"]);
+            //Ausgabe des Eintrags mit Key 'Ciao'
+            Console.WriteLine(Dict["Ciao"]);
         }
     }
 }
